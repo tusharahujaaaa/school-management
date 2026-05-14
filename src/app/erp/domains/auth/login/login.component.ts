43ce5904c363cb0113cff0169828a4cc1ad757ce
Tushar <tushar.ahuja@netprohetsglobal.com>
2026-05-14 10:55:21 +0530

    removed the unwanted codeof sakai theme

--- src/app/erp/domains/auth/login/login.component.ts
+++ src/app/erp/domains/auth/login/login.component.ts
@@ -9,12 +9,12 @@ import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { RippleModule } from 'primeng/ripple';
 import { PasswordModule } from 'primeng/password';
-import { AppFloatingConfigurator } from '../../../../layout/component/app.floatingconfigurator';
+
 import { ProgressBarModule } from 'primeng/progressbar';
 @Component({
   selector: 'app-erp-login',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule, InputTextModule, ButtonModule, CheckboxModule, RippleModule, AppFloatingConfigurator, PasswordModule, ProgressBarModule],
+  imports: [CommonModule, RouterModule, FormsModule, InputTextModule, ButtonModule, CheckboxModule, RippleModule, PasswordModule, ProgressBarModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
