3c35a77b297aed6f27653f6d87f9d7a98221af73
Tushar <ahujav136@gmail.com>
2026-07-10 13:53:34 +0530

    chore: setup ngx-translate and api constants

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,5 @@
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch, withInterceptors, HttpClient } from '@angular/common/http';
+import { ApplicationConfig, provideZonelessChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
@@ -8,6 +8,12 @@ import { appRoutes } from './app.routes';
 import { authInterceptor } from './app/erp/core/api/interceptors/auth.interceptor';
 import { loadingInterceptor } from './app/erp/core/api/interceptors/loading.interceptor';
 import { errorInterceptor } from './app/erp/core/api/interceptors/error.interceptor';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -18,6 +24,16 @@ export const appConfig: ApplicationConfig = {
         ),
         provideZonelessChangeDetection(),
         providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
-        MessageService // Required for global error handling toasts
+        MessageService, // Required for global error handling toasts
+        importProvidersFrom(
+            TranslateModule.forRoot({
+                defaultLanguage: 'en',
+                loader: {
+                    provide: TranslateLoader,
+                    useFactory: HttpLoaderFactory,
+                    deps: [HttpClient]
+                }
+            })
+        )
     ]
 };
